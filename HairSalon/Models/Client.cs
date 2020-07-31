05b74b99c8209c13a67799d60b8034f968519f96
Ben White <bwhite2140@outlook.com>
2020-07-31 10:54:37 -0700

    add stylist and client class

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,8 +2,12 @@ namespace HairSalon.Models
 {
   public class Client
   {
+    public Client()
+    {
+      this.Stylists = new HashSet<Stylist>();
+    }
     public int ClientId { get; set; }
     public string Name { get; set; }
-    
+    public virtual ICollection Stylists { get; set; }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -2,6 +2,8 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    
+    public int StylistId { get; set; }
+    public int ClientId { get; set; }
+    public string Name { get; set; }
   }
 }
